8af29093f05a3d68f64c4ceff3bed4118e59a6fa
Dave <dave@stackden.com>
2018-03-27 18:39:39 -0700

    Created Breadcrumb, Header, Footer and Loading Components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ImageGallery from '../image_gallery/imageGallery.js';
 import './app.css';
+import Loading from '../loading/loading';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import Breadcrumbs from '../breadcrumbs/breadcrumbs';
+
 
 const App = () => (
-    <div className='mainContainer'>                    
+    <div className='mainContainer'>
+        <Header/>
+        <Breadcrumbs/>                    
         <h1>Part Pig</h1>
         <ImageGallery showList={true} mainImage ='part1/subaruWheelsMain1.jpg' imagesList = {['part1/subaruWheelsMain1.jpg','part1/subaruWheels2.jpg','part1/subaruWheels3.jpg','part1/subaruWheels4.jpg']} />       
+    
+        <Footer/>  
     </div>
 );
 
---
+++ src/components/breadcrumbs/breadcrumbs.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './breadcrumbs.css';
+
+const Breadcrumbs = () => (
+ <div className="breadCrumbs"> Back to search results | Exhaust > Catback > Invidia </div>
+);
+
+export default Breadcrumbs;
