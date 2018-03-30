79f0cf0bdf505530d247181490dcc6723e336817
Li Zhang <bylizhang@gmail.com>
2018-03-29 22:01:01 -0700

    pulled dev after Dave's modification 2018-03-29

--- src/assets/dummyData/dummySql/part.sql
+++ src/assets/dummyData/dummySql/part.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 25, 2018 at 01:52 AM
+-- Generation Time: Mar 26, 2018 at 06:34 PM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.1.7
 
@@ -29,36 +29,38 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `part` (
-  `id` mediumint(8) UNSIGNED NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `description` varchar(300) NOT NULL,
-  `part_condition` enum('Excellent','Good','Fair') DEFAULT NULL,
-  `status` enum('In stock','Out of stock','Backordered') NOT NULL,
-  `brand` varchar(30) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
+  `part_name` varchar(50) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `part_condition` tinyint(3) UNSIGNED DEFAULT NULL,
+  `status` enum('For sale','In storage','Sold','Shipped') NOT NULL,
+  `brand` varchar(30) DEFAULT NULL,
   `make` varchar(30) NOT NULL,
   `model` varchar(30) NOT NULL,
   `year` year(4) NOT NULL,
-  `seller_id` mediumint(8) UNSIGNED NOT NULL,
-  `buyer_id` mediumint(8) UNSIGNED NOT NULL,
-  `part_number` varchar(40) NOT NULL,
-  `sku` varchar(40) NOT NULL,
+  `seller_id` int(10) UNSIGNED NOT NULL,
+  `listing_ID` varchar(300) DEFAULT NULL,
+  `buyer_id` int(10) UNSIGNED DEFAULT NULL,
+  `part_number` varchar(40) DEFAULT NULL,
+  `sku` varchar(40) DEFAULT NULL,
   `price_usd` decimal(10,3) NOT NULL,
-  `msrp_usd` decimal(10,3) NOT NULL,
-  `weight_lb` decimal(9,4) NOT NULL,
-  `width_inch` decimal(6,3) NOT NULL,
-  `height_inch` decimal(6,3) NOT NULL,
-  `depth_inch` decimal(6,3) NOT NULL,
+  `msrp_usd` decimal(10,3) DEFAULT NULL,
+  `weight_lb` decimal(9,4) DEFAULT NULL,
+  `width_inch` decimal(6,3) DEFAULT NULL,
+  `height_inch` decimal(6,3) DEFAULT NULL,
+  `depth_inch` decimal(6,3) DEFAULT NULL,
+  `original_purchase_date` date DEFAULT NULL,
   `insert_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `sold_date` datetime NOT NULL,
-  `image` varchar(60) NOT NULL
+  `image` varchar(300) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `part`
 --
 
-INSERT INTO `part` (`id`, `name`, `description`, `part_condition`, `status`, `brand`, `make`, `model`, `year`, `seller_id`, `buyer_id`, `part_number`, `sku`, `price_usd`, `msrp_usd`, `weight_lb`, `width_inch`, `height_inch`, `depth_inch`, `insert_date`, `sold_date`, `image`) VALUES
-(1, 'Roctection Hitch Mounted Mud Flaps', 'Access Rockstar Roctection Universal (Fits Most P/Us & SUVs) 80in. Wide Hitch Mounted Mud Flaps', 'Excellent', 'In stock', 'Agricover', 'Toyota', 'Camry', 2018, 1, 3, 'accC100001', 'C100001', 209.250, 279.000, 28.0000, 33.000, 13.000, 5.000, '2018-03-25 01:51:31', '0000-00-00 00:00:00', '');
+INSERT INTO `part` (`id`, `part_name`, `description`, `part_condition`, `status`, `brand`, `make`, `model`, `year`, `seller_id`, `listing_ID`, `buyer_id`, `part_number`, `sku`, `price_usd`, `msrp_usd`, `weight_lb`, `width_inch`, `height_inch`, `depth_inch`, `original_purchase_date`, `insert_date`, `sold_date`, `image`) VALUES
+(1, 'Roctection Hitch Mounted Mud Flaps', 'Access Rockstar Roctection Universal (Fits Most P/Us & SUVs) 80in. Wide Hitch Mounted Mud Flaps', 1, '', 'Agricover', 'Toyota', 'Camry', 2018, 1, NULL, 3, 'accC100001', 'C100001', 209.250, 279.000, 28.0000, 33.000, 13.000, 5.000, NULL, '2018-03-25 01:51:31', '0000-00-00 00:00:00', '');
 
 --
 -- Indexes for dumped tables
@@ -78,7 +80,7 @@ ALTER TABLE `part`
 -- AUTO_INCREMENT for table `part`
 --
 ALTER TABLE `part`
-  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
