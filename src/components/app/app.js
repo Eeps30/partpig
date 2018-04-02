30948bd576594f4d75bc6ddcb0671e87f6ce1fbc
Dave <dave@stackden.com>
2018-04-02 16:44:58 -0700

    Fixed navbar bugs and login page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,7 +13,7 @@ import PartDetails from '../part/partDetails/partDetails';
 import About from '../about/about';
 import Contact from '../contact/contact';
 import SellPart from '../sellpart/sellpart';
-import Login from '../login/loginForm/loginForm';
+import Login from '../login/login';
 
 
 class App extends Component{
