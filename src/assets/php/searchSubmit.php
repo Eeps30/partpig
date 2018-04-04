4571a2fc270a9e342ddf4fee5fefb5288fdc1177
briantmorr <briantmorr@gmail.com>
2018-04-04 10:56:49 -0700

    searchsubmit endpoint working with year, make, model, and default

--- src/assets/php/searchSubmit.php
+++ src/assets/php/searchSubmit.php
@@ -1,25 +1,32 @@
 <?php
 require_once('mysqlConnect.php');
-if(isset($_POST['make'])){
-    if(!empty($_POST['make'])){
-        $make = $_POST['make'];
-    }
-    else{
-        $output['error'][] = 'SET THE MAKE';
-        die;
-    }
-}
-else{
-    $make = 'Toyota';
-}
-$query =  "SELECT * FROM `part` AS p WHERE p.make='$make'";
-        
-$result = mysqli_query($conn, $query);
 $output = [
     'success'=> false,
     'error' => [],
     'data' => []
 ];
+// default query
+$query =  "SELECT p.id, p.brand, p.part_name AS title, p.id AS category, p.make, p.model, p.year, p.part_number AS partNumber, p.price_usd AS price  FROM `part` AS p ";   
+
+if(isset($_POST['make']) && isset($_POST['model']) && isset($_POST['year'])){
+    if(!empty($_POST['make']) && !empty($_POST['model'])&& !empty($_POST['year'])){
+        $make = $_POST['make'];
+        $model = $_POST['model'];
+        $year = $_POST['year'];
+
+        $query =  "SELECT p.id, p.brand, p.part_name AS title, p.id AS category, p.make, p.model, p.year, p.part_number AS partNumber, p.price_usd AS price  FROM `part` AS p  WHERE `make`='$make' AND `model`='$model' AND `year`='$year'";
+    }
+    else{
+        $output['error'][] = 'Make or Model or Year empty';
+    }
+}
+else{
+    $output['error'][] = 'Make or Model or Year unset';
+}
+
+        
+$result = mysqli_query($conn, $query);
+
 if($result){
     if(mysqli_num_rows($result)> 0){
         while($row = mysqli_fetch_assoc($result)){
