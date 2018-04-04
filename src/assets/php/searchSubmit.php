c79bed3805281ea0de2446c5044c59bcfa6bb90b
briantmorr <briantmorr@gmail.com>
2018-04-04 16:30:13 -0700

    tweaked search, working on image column in database

--- src/assets/php/searchSubmit.php
+++ src/assets/php/searchSubmit.php
@@ -6,7 +6,7 @@ $output = [
     'data' => []
 ];
 // default query
-$query =  "SELECT p.id, p.brand, p.part_name AS title, p.id AS category, p.make, p.model, p.year, p.part_number AS partNumber, p.price_usd AS price  FROM `part` AS p ";   
+$query =  "SELECT p.id, p.brand, p.part_name AS title, p.id AS category, p.make, p.model, p.year, p.part_number AS partNumber, p.price_usd AS price, p.image AS images  FROM `part` AS p ";   
 
 if(isset($_POST['make']) && isset($_POST['model']) && isset($_POST['year'])){
     if(!empty($_POST['make']) && !empty($_POST['model'])&& !empty($_POST['year'])){
@@ -14,7 +14,7 @@ if(isset($_POST['make']) && isset($_POST['model']) && isset($_POST['year'])){
         $model = $_POST['model'];
         $year = $_POST['year'];
 
-        $query =  "SELECT p.id, p.brand, p.part_name AS title, p.id AS category, p.make, p.model, p.year, p.part_number AS partNumber, p.price_usd AS price  FROM `part` AS p  WHERE `make`='$make' AND `model`='$model' AND `year`='$year'";
+        $query =  "SELECT p.id, p.brand, p.part_name AS title, p.id AS category, p.make, p.model, p.year, p.part_number AS partNumber, p.price_usd AS price, p.image AS images  FROM `part` AS p  WHERE `make`='$make' AND `model`='$model' AND `year`='$year'";
     }
     else{
         $output['error'][] = 'Make or Model or Year empty';
