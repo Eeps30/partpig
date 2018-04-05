f8cad3acb79e391396fce2bef81274cfab853493
briantmorr <briantmorr@gmail.com>
2018-04-05 12:28:25 -0700

    php changes year and price to numbers in searchSubmit results

--- src/assets/php/searchSubmit.php
+++ src/assets/php/searchSubmit.php
@@ -8,7 +8,7 @@ $output = [
     'data' => []
 ];
 // default query
-$query =  "SELECT p.id, p.brand, p.part_name AS title, p.id AS category, p.make, p.model, p.year, p.part_number AS partNumber, p.price_usd AS price, p.image AS images  FROM `part` AS p ";   
+$query =  "SELECT p.id, p.brand, p.part_name AS title, p.id AS category, p.make, p.model, p.year, p.part_number AS partNumber, p.price_usd AS price, i.url  FROM `part` AS p JOIN `image` AS i ON p.id=i.part_id";   
 
 // checks if fields are set and queries specific make/model/year
 if(isset($_POST['make']) && isset($_POST['model']) && isset($_POST['year'])){
@@ -38,6 +38,8 @@ if($result){
     if(mysqli_num_rows($result)> 0){
         while($row = mysqli_fetch_assoc($result)){
             $row['display'] = $display;
+            $row['price'] = (float)$row['price'];
+            $row['year'] = (int)$row['year'];
             $output['data'][] = $row;
         }
     }
