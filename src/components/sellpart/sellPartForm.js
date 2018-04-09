a21a3c2e9f06373eff0922a874c3369d7a738888
Alejandro Gaspar <caxpper@hotmail.com>
2018-04-09 15:16:08 -0700

    fixed height in the body

--- src/components/sellpart/sellPartForm.js
+++ src/components/sellpart/sellPartForm.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import "./sellpart.css";
-import parts from '../part/partsData';
 import ImageUploader from '../imageUploader/imageUploader';
 import axios from 'axios';
 
