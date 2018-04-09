7e59a9e732d4fbe6f051e153162e18f5122a12ee
Li Zhang <bylizhang@gmail.com>
2018-04-09 09:33:38 -0700

    edited config file

--- src/assets/php/listPart.php
+++ src/assets/php/listPart.php
@@ -6,17 +6,17 @@ require("sanitizeInput.php");
 //only do filter_var for email and phone
 
 //hard-coded test $_POST data
-// $_POST['part_name'] = ' 2nd test/<?\\\<Post>  ';
-// $_POST['description'] = '    ';
-// $_POST['part_condition'] = '1 -- Heavily used';
-// $_POST['status'] = 'For sale';
-// $_POST['brand'] = 'testBrand';
-// $_POST['make'] = 'testMake';
-// $_POST['model'] = 'testModel';
-// $_POST['year'] = '2018';
-// $_POST['seller_id'] = 1;
-// $_POST['price_usd'] = 999;
-// $_POST['listed_date'] = date("Y-m-d", time());
+$_POST['part_name'] = ' 3rd test/<?\\\<Post>  ';
+$_POST['description'] = '    ';
+$_POST['part_condition'] = '1 -- Heavily used';
+$_POST['status'] = 'For sale';
+$_POST['brand'] = 'testBrand';
+$_POST['make'] = 'testMake';
+$_POST['model'] = 'testModel';
+$_POST['year'] = '2018';
+$_POST['seller_id'] = 1;
+$_POST['price_usd'] = 999;
+$_POST['listed_date'] = date("Y-m-d", time());
 // remove above content for frontEnd testing
 
 $fieldsToSanitize = ['part_name', 'description', 'part_condition', 'brand', 'make', 'model', 'year', 'seller_id', 'price_usd', 'listed_date'];
@@ -44,8 +44,6 @@ $result = mysqli_query($conn, $query);
 $rows_affected = mysqli_affected_rows($conn);
 $data = json_encode($result);
 if($result){
-	print($data);
-
 	echo "New record created successfully. Total rows affected: ", $rows_affected .".";
 } else {
 	echo "Error: " . mysqli_error($conn);
