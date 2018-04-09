14e25190aa224c56001c1afb067c23c22d0d4066
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-04-08 17:25:40 -0700

    refactored listPart.php to use array for input fields

--- src/assets/php/searchSubmit.php
+++ src/assets/php/searchSubmit.php
@@ -10,36 +10,45 @@ $output = [
 $_GET['make']='subaru';
 
 // default query
-$query =  "SELECT p.id, p.brand, p.part_name AS title, p.id AS category, p.make, p.model, p.year,                               
-                  p.part_number AS partNumber, p.price_usd AS price, i.url AS images
+// p.id AS category ???
+$query =  "SELECT p.id, 
+                  p.brand, 
+                  p.part_name AS title, 
+                  p.id AS category, 
+                  p.make, 
+                  p.model, 
+                  p.year,                               
+                  p.part_number AS partNumber, 
+                  p.price_usd AS price, 
+                  i.url AS images
             FROM `part` AS p 
             JOIN `image` AS i 
-            ON i.id=
-            (
-                SELECT MIN(im.id) 
-                FROM `image` as im 
-                WHERE im.part_id=p.id
+                ON i.id=
+                (
+                    SELECT MIN(im.id) 
+                    FROM `image` as im 
+                    WHERE im.part_id=p.id
                 )";   
 
-$partsToCheck = ['make', 'model', 'year'];
+$fieldsToCheck = ['make', 'model', 'year'];  //changed name from partsToCheck
 $subQuery = [];
-forEach($partsToCheck as $value){
+
+forEach($fieldsToCheck as $value){
     if(!empty($_GET[$value])){
         $subQuery[] = " $value = '{$_GET[$value]}'";
     }
-    
 }
+
 if(count($subQuery)>0){
 	$query .= " WHERE ". implode(" AND ",$subQuery);
 }
 
-        
 $result = mysqli_query($conn, $query);
 // make a display object that we later add to each search result
 $display = new stdClass();
 $display->brand = 'true';
 $display->price = 'true';
-$ar = [];
+$ar = []; //@Brian: was $ar the old version of $row['images']? should we remove it? --Li, 04/08/18
 if($result){
     if(mysqli_num_rows($result)> 0){
         while($row = mysqli_fetch_assoc($result)){
