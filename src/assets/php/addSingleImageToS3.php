1bb490296b48f86c4f5df929879e69a04e5edbe9
briantmorr <briantmorr@gmail.com>
2018-04-10 14:25:17 -0700

    made aws file naming more dynamic

--- src/assets/php/addSingleImageToS3.php
+++ src/assets/php/addSingleImageToS3.php
@@ -6,17 +6,19 @@ $sdk = new Aws\Sdk($sharedConfig);
 
 //defaults if post doesn't have user or image
 if(empty($_POST['username'])){
-    $_POST['username'] = 'user1';
+    $_POST['username'] = 'user2';
 }
 if(empty($_POST['image'])){
     $_POST['image'] = '../images/part2/part2a.jpg';
 }
 
 $day = date('Y-m-d');
-$unixTimestamp = (new DateTime())->format('U');
+// $unixTimestamp = (new DateTime())->format('U');
 $username = $_POST['username'];
 $image = $_POST['image'];
-
+$filePath = "images/$day/$username";
+$previousFileName = 'listedImageName';
+$fileName = $previousFileName . time();
 
 
 
@@ -24,7 +26,7 @@ $s3Client = $sdk->createS3();
 try{
     $result =  $s3Client->putObject([
         'Bucket' => 'teampartpig',
-        'Key'    => "images/$day/$username/$unixTimestamp" ,
+        'Key'    => "$filePath/$fileName" ,
         'Body'   => fopen($image, 'r'),
         'ACL'    => 'public-read',
         ]);
