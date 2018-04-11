b12a5f74b4004ce1665de0f66207add464105046
briantmorr <briantmorr@gmail.com>
2018-04-10 19:52:02 -0700

    slightly edited login form for endpoint call

--- src/components/login/loginForm/loginForm.js
+++ src/components/login/loginForm/loginForm.js
@@ -4,11 +4,11 @@ import "./loginForm.css";
 const LoginForm = () => (
     <div className="loginForm">
         <h2>Login with email address</h2>
-            <form>
+            <form action="login.php" method="post">
                 <label>Email address</label>
                 <input type="text" id="email" name="email" placeholder="Email address"/>
                 <label>Password</label>
-                <input type="text" id="password" name="password" placeholder="Password"/>
+                <input type="password" id="password" name="password" placeholder="Password"/>
                 <input type="submit" value="Login"/>
             </form>
     </div>
