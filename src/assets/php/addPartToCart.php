1069b2ba8b7e2625fd22357e7e3be6d40ccb478c
Li Zhang <bylizhang@gmail.com>
2018-04-12 15:49:50 -0700

    first order received in database from addPartToCart.php

--- src/assets/php/addPartToCart.php
+++ src/assets/php/addPartToCart.php
@@ -31,12 +31,23 @@ else{
     $output['error'][] = 'Error in database query, probably problem with enum letters';
 }
 
-$query2 = "INSERT INTO `order_meta`
-            
-            
-        ";
-
-
 $json_output = json_encode($output);
 print($json_output);
+
+$query2 = "INSERT INTO `order_meta`
+            (buyer_id, count, subtotal, tax, status, address_id, shipping_charge)
+           Values (
+             2, 1, 55, (55*0.075), 'Order received', 1, 9.99
+           )          
+        ";
+
+$result2 = mysqli_query($conn, $query2);
+$rows_affected = mysqli_affected_rows($conn);
+if($result2){
+    $last_id = mysqli_insert_id($conn);
+    echo "New record created successfully in order_meta. Total rows affected: ", $rows_affected ."." . " Last inserted ID is: ". $last_id . ".";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+mysqli_close($conn);
 ?>
