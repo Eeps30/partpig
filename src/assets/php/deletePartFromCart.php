bfe3befe511319d8e0b9fd5bfe046128a4e26c56
Li Zhang <bylizhang@gmail.com>
2018-04-12 15:40:49 -0700

    start to make endpoints for add and delete part to from cart

--- src/assets/php/deletePartFromCart.php
+++ src/assets/php/deletePartFromCart.php
@@ -16,26 +16,20 @@ else{
     $id = $_GET['id'];  
 }
 
-$status = 'incart';
+$status = 'available';
 
-$query1 = "UPDATE `part` 
-           SET `status` = '$status' 
-           WHERE `part`.`id` = $id"; 
+$query = "UPDATE `part` 
+          SET `status` = '$status' 
+          WHERE `part`.`id` = $id"; 
 
-$result1 = mysqli_query($conn, $query1);
-if($result1){
+$result = mysqli_query($conn, $query);
+if($result){
     $output['success'] = true;
-    $output['data'][] = "part '$id' status updated to $status";
+    $output['data'] = "part '$id' status updated to $status";
 }
 else{
     $output['error'][] = 'Error in database query, probably problem with enum letters';
 }
-
-$query2 = "INSERT INTO `order_meta`
-            
-            
-        ";
-
 
 $json_output = json_encode($output);
 print($json_output);
