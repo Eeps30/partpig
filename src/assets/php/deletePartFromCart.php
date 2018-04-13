17b9761572c5e598cd0458545af1ba56e14a5796
Li Zhang <bylizhang@gmail.com>
2018-04-12 17:29:18 -0700

    small formatting edit

---
+++ src/assets/php/deletePartFromCart.php
@@ -0,0 +1,53 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+require_once('mysqlConnect.php');
+//basic output format, all data gets pushed into data[]
+$output = [
+    'success'=> false,
+    'error' => [],
+    'data' => []
+];
+
+$_GET['part_id'] = 2;
+$_GET['user_id'] = 2;
+
+if(!isset($_GET['part_id'])){
+    $output['error'][] = 'Error: user id not specified';
+}
+else{
+    $part_id = $_GET['part_id'];  
+}
+
+$status = 'available';
+
+$query = "UPDATE `part` 
+          SET `status` = '$status' 
+          WHERE `part`.`id` = $part_id"; 
+
+$result = mysqli_query($conn, $query);
+if($result){
+    $output['success'] = true;
+    $output['data'] = "part '$part_id' status updated to $status";
+}
+else{
+    $output['error'][] = 'Error in database query, probably problem with enum letters';
+}
+
+$json_output = json_encode($output);
+print($json_output);
+
+$buyer_id = $_GET['user_id'];
+
+$query2 = "DELETE FROM `order_meta`
+           WHERE `order_meta`.`buyer_id` = '$buyer_id'
+           ";
+
+$result2 = mysqli_query($conn, $query2);
+$rows_affected = mysqli_affected_rows($conn);
+if($result2){
+    echo "Successfully removed records from table order_meta. Total rows affected: ", $rows_affected .".";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
