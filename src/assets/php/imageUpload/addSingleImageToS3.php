5ea9d79fe2201cb6c7bcce8b0e3b59480d44d3e5
briantmorr <briantmorr@gmail.com>
2018-04-13 11:38:59 -0700

    upload with multiple image types working

--- src/assets/php/imageUpload/addSingleImageToS3.php
+++ src/assets/php/imageUpload/addSingleImageToS3.php
@@ -1,8 +1,33 @@
 <?php
 require './vendor/autoload.php';
-require('./awsConnect.php');
+require('./imageUpload/awsConnect.php');
 
 $sdk = new Aws\Sdk($sharedConfig);
+
+
+function handleDifferentImageTypes($imageURI){
+    global $ext;
+    if(preg_match('/data:image\/png/', $imageURI)){
+        echo "match png";   
+        $imageURI = str_replace('data:image/png;base64,', '', $imageURI);
+        $ext = 'png';
+       }
+    else if(preg_match('/data:image\/jpeg/', $imageURI)){
+        echo "match jpeg";  
+        $imageURI = str_replace('data:image/jpeg;base64,', '', $imageURI); 
+        $ext = 'jpeg';
+    }
+    else if(preg_match('/data:image\/jpg/', $imageURI)){
+        echo "match jpg";  
+        $imageURI = str_replace('data:image/jpg;base64,', '', $imageURI); 
+        $ext = 'jpg';
+    }
+    else{
+        die("invalid file type");
+    }
+    return $imageURI;
+}
+
 
 //defaults if post doesn't have user or image
 if(empty($_POST['username'])){
@@ -19,25 +44,27 @@ $filePath = "images/$day/$username";
 $previousFileName = 'listedImageName';
 $fileName = $previousFileName . time();
 
-define('UPLOAD_DIR', './uploads/');
+define('UPLOAD_DIR', './imageUpload/tempUploads/');
 $img = $request_data['images'][0];
-$img = str_replace('data:image/png;base64,', '', $img);
+$ext = '';
+
+$img = handleDifferentImageTypes($img);
 $data = base64_decode($img);
-$file = UPLOAD_DIR.$fileName.'.png';
-$success = file_put_contents($file,$data);
+$tempFilePath = UPLOAD_DIR.$fileName.$ext;
+$success = file_put_contents($tempFilePath,$data);
 
 if(!$success){
 die('unable to save file');
 }
 
-
+ $fileContents = file_get_contents("$tempFilePath");
 
 $s3Client = $sdk->createS3();
 try{
     $result =  $s3Client->putObject([
         'Bucket' => 'teampartpig',
         'Key'    => "$filePath/$fileName" ,
-        'Body'   => fopen($file, 'r'),
+        'Body'   => fopen($tempFilePath, 'r'),
         'ACL'    => 'public-read',
         ]);
 
@@ -52,7 +79,7 @@ try{
     echo "There was an error uploading the file.\n";
 }
 
-
+require_once('./imageUpload/deleteFileFromTemp.php');
 $imageUrl = $result['ObjectURL'];
 
 
