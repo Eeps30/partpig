364de65ba228333bebbf204d7b0d0a85e023e2f6
unknown <briantmorr@gmail.com>
2018-04-18 12:33:36 -0700

    mail edits

--- src/assets/php/Mail/transactionalEmail.php
+++ src/assets/php/Mail/transactionalEmail.php
@@ -1,16 +1,38 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 require("mailDetails.php");
+
+
+$output = [
+    'success'=> false,
+    'error' => [],
+    'data' => []
+];
+
+$entityBody = file_get_contents('php://input');
+$request_data = json_decode($entityBody, true);
+
+$requiredFields = ['name', 'email', 'body', 'subject'];
+foreach($request_data as $key => $value){
+    if(in_array($key, $requiredFields)){
+       $$key = $value;
+    }
+    else{
+        die('required fields missing');
+    }
+}
+
 // using SendGrid's PHP Library
 // https://github.com/sendgrid/sendgrid-php
 // If you are using Composer (recommended)
+
 require '../vendor/autoload.php';
 // If you are not using Composer
 // require("path/to/sendgrid-php/sendgrid-php.php");
-$from = new SendGrid\Email("PartPig Automated Response", "$sender");
-$subject = "Sending with SendGrid is Fun";
-$to = new SendGrid\Email("User", "$destination");
-$content = new SendGrid\Content("text/plain", "refactoring again");
+$from = new SendGrid\Email("$name", "$email");
+// $subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("PartPig Automated Contact", "$destination");
+$content = new SendGrid\Content("text/plain", "$body");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 // getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,43 +1,151 @@
-import React from "react";
+import React, { Component } from "react";
+// import './checkout.css';
+// import {Link} from 'react-router-dom';
+// import Field from './field';
+// import Loading from '../loading/loading';
+import axios from 'axios';
+// import inputs from './fieldsData';
+
+
+// import React from "react";
 import './contact.css';
 
-const Contact = () => (
-        <div>
-            <div className="contact-us">
-            	<h1>CONTACT US</h1>
-            </div>
-            <div className='container contactContainer'>
-				<div className="contact-header">
-				    <h3>Have a question? Drop us a line!</h3>
-				    <h3>Please submit your inquiry through the contact form.</h3>
-				    <h3>We are committed to providing you the best part-purchasing experience!</h3>
+class Contact extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			form:{
+				name: "",
+				email: "",
+				subject: "",
+				body: ""
+			}
+		}
+	}
+	render() {
+		console.log(this.state);
+		// const fielields = inputs.map(((field, index) => {                    
+        //     return <Field key={index} {...field} error={this.state.shippingErrors[field.name]} handleOnBlur={this.shippinghandleOnBlur.bind(this)} handleInputChange={this.handleInputChange} value={this.state.shippingAddress[field.name] || ''}/>
+        // }).bind(this));
+
+		let submitButton = <button type="button" onClick={e=>this.handleSubmit(e)} className='button-link'>Submit Your Message</button>
+		// if(!(Object.keys(this.state.shippingErrors).length === 0) || !(Object.keys(this.state.billingErrors).length === 0)){
+		//     submitButton = <button onClick={e => e.preventDefault()} className='disabled'>Submit Your Message</button>;
+		// }
+		const {name, email, subject, body} = this.state.form;
+		return (
+			<div>
+				<div className="contact-us">
+					<h1>CONTACT US</h1>
 				</div>
-				<form className="contactForm">
-				    <div className="input-container">
-				        <label htmlFor='name'>Name:</label>
-				        <input type="text" className="name" placeholder="First Name, Last Name"/>
-				    </div>
-				    <div className="input-container">
-				        <label>Email:</label>
-				        <input type="text" className="email" placeholder="Enter your email address here"/>
-				    </div>
-				    <div className="input-container">
-				        <label>Subject:</label>
-				        <input type="text" className="subject" placeholder="Enter a subject"/>
-				    </div>
-				    <div className="input-container">
-				        <label>Message:</label>
-				        <textarea className="message" placeholder="Enter your message here"></textarea>
-				    </div>
-				    <button type="button" className="button-link">Submit Your Message</button>
-				</form>
+				<div className='container contactContainer'>
+					<div className="contact-header">
+						<h3>Have a question? Drop us a line!</h3>
+						<h3>Please submit your inquiry through the contact form.</h3>
+						<h3>We are committed to providing you the best part-purchasing experience!</h3>
+					</div>
+					<form className="contactForm">
+						<div className="input-container">
+							<label htmlFor='name'>Name:</label>
+							<input type="text" className="name" value={name} placeholder="First Name, Last Name" onChange={this.handleNameInputChange.bind(this)}/>
+						</div>
+						<div className="input-container">
+							<label>Email:</label>
+							<input type="text" className="email" value={email} placeholder="Enter your email address here" onChange={this.handleEmailInputChange.bind(this)}/>
+						</div>
+						<div className="input-container">
+							<label>Subject:</label>
+							<input type="text" className="subject" value={subject}placeholder="Enter a subject" onChange={this.handleSubjectInputChange.bind(this)}/>
+						</div>
+						<div className="input-container">
+							<label>Message:</label>
+							<textarea className="message" value={body} placeholder="Enter your message here"onChange={this.handleMessageInputChange.bind(this)}></textarea>
+						</div>
+						{submitButton}
+
+					</form>
+				</div>
+				<div className="modal-overlay"></div>
+				<div className="modal">
+					<div className="close"></div>
+					<label></label>
+				</div>
 			</div>
-			<div className="modal-overlay"></div>
-			<div className="modal">
-			    <div className="close"></div>
-			    <label></label>
-			</div>
-        </div>    
-);
+		)
+	}
+	reset(){
+		console.log('reseting?');
+		this.state ={
+			form:{
+				name: "",
+				email: "",
+				subject: "",
+				body: ""
+			}
+		};
+	}
+	handleSubmit() {
+		event.preventDefault();
+
+		const url = 'http://localhost:8000/teampartpig/src/assets/php/Mail/transactionalEmail.php';
+		 const data  = {...this.state.form};
+		 this.reset();
+		 console.log("data is", data);
+		axios({
+			url: url,
+			method: 'post',
+			data: data,
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}}).then(resp => {
+				console.log("Server Response:", resp);
+			}).catch(err => {
+				console.log("There was an error:");
+			});
+	}
+	handleNameInputChange(e){
+		const{value} = e.target
+		this.setState({
+			form:{
+				...this.state.form,
+				name:value
+			}
+		});
+	}
+	handleEmailInputChange(e){
+		const{value} = e.target
+		console.log(value);
+		// console.log('inputchange', e.target.value);
+		this.setState({
+			form:{
+				...this.state.form,
+				email:value
+			}
+		});
+	}
+	handleSubjectInputChange(e){
+		const{value} = e.target
+		console.log(value);
+		// console.log('inputchange', e.target.value);
+		this.setState({
+			form:{
+				...this.state.form,
+				subject:value
+			}
+		});
+	}
+	handleMessageInputChange(e){
+		const{value} = e.target
+		this.setState({
+			form:{
+				...this.state.form,
+				body:value
+			}
+		});
+	}
+	
+}
 
 export default Contact
+
