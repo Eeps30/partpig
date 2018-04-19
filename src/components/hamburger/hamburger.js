52e94e54a9fcf02c6c5aa32df0b87b97c15cd14f
Alejandro Gaspar <caxpper@hotmail.com>
2018-04-19 11:20:26 -0700

    logout implemented

--- src/components/hamburger/hamburger.js
+++ src/components/hamburger/hamburger.js
@@ -2,21 +2,34 @@ import React from "react";
 import './hamburger.css';
 import {Link} from 'react-router-dom';
 
-const Hamburger = () => (
-      <div id="menuToggle">
-        <input type="checkbox"/>
-        <span></span>
-        <span></span>
-        <span></span>
-        <ul id="menu">
-          <Link to="/"><li>Home</li></Link>
-          <Link to="/dashboard"><li>User Dashboard</li></Link>
-          <Link to="/partresults"><li>Part Results</li></Link>
-          <Link to="/sellpart"><li>Sell a Part</li></Link>
-          <Link to="/about"><li>About us</li></Link>
-          <Link to="/contact"><li>Contact</li></Link>
-        </ul>
-      </div>
-);
+const Hamburger = (props) => {
+
+    function hideMenu(){
+
+    }
+
+    let dashBoard = <Link onCLick={hideMenu} to="/dashboard"><li>User Dashboard</li></Link>
+    let sellPartIcon = <Link onCLick={hideMenu} to="/sellpart"><li>Sell a Part</li></Link>  
+    if(!props.userId){
+        dashBoard = <Link onCLick={hideMenu} to="/login"><li>User Dashboard</li></Link>
+        sellPartIcon = <Link onCLick={hideMenu} to="/login"><li>Sell a Part</li></Link> 
+    }
+
+    return(
+          <div id="menuToggle">
+            <input type="checkbox"/>
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id="menu">
+              <Link onCLick={hideMenu} to="/"><li>Home</li></Link>
+              {dashBoard}
+              {sellPartIcon}
+              <Link onCLick={hideMenu} to="/about"><li>About us</li></Link>
+              <Link onCLick={hideMenu} to="/contact"><li>Contact</li></Link>
+            </ul>
+          </div>
+    );
+}
 
 export default Hamburger;
