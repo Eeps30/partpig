f6a4ceccf56aee6b0ae532c64d87a88bff6c2383
unknown <briantmorr@gmail.com>
2018-04-19 15:42:49 -0700

    1st round of cleaning up backend files

--- src/assets/php/listNewPart/addImagesToS3.php
+++ src/assets/php/listNewPart/addImagesToS3.php
@@ -8,8 +8,8 @@ require('s3Functions.php');
 
 $sdk = new Aws\Sdk($sharedConfig);
 
-if(empty($request_data['username'])){
-    $request_data['username'] = 'anonymous';
+if(empty($request_data['seller'])){
+    $request_data['seller'] = 'anonymous';
 }
 if(empty($request_data['images'][0])){
     die('please upload an image');
@@ -17,7 +17,7 @@ if(empty($request_data['images'][0])){
 
 $GLOBALS['ext'] = '';
 $day = date('Y-m-d');
-$username = $request_data['username'];
+$username = $request_data['seller'];
 
 // compose the file directory path ex: images/2018-4-12/user1/
 $filePath = "images/$day/$username";
