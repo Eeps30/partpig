d5f503d57f3830468d79c30f0e6ce814943f278e
Eeps30 <eeps30@gmail.com>
2018-04-24 21:31:32 -0700

    added axios call to sign up

--- src/components/signUp/signUp.js
+++ src/components/signUp/signUp.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './signUp.css';
+import axios from 'axios';
 
 class SignUp extends Component {
     constructor(props){
@@ -41,6 +42,29 @@ class SignUp extends Component {
 
     onSubmit(event){
         event.preventDefault();
+
+        const { username, email, password } = this.state
+        const params = {
+            username, email, password 
+        }
+
+        if(this.state.password === this.state.confirmPass){
+            axios({
+                url: 'http://localhost:8000/teampartpig/src/assets/php/login/newUserSignup.php',
+                method: 'post',
+                data: params,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(resp => {
+                if(resp.data.success){
+                    console.log(resp.data.success)
+                }
+            }).catch(err => {
+                console.log('error is: ', err);
+            });
+        }
+        
     }
 
     render(){
@@ -51,13 +75,13 @@ class SignUp extends Component {
                         <form>
                             <h2>Create an Account</h2>
                             <label>Email Address:</label>
-                            <input value={this.state.email} onChange={this.handleChange.bind(this)} type="text"/>
+                            <input value={this.state.email} onChange={this.handleChange.bind(this)} required="" type="text"/>
                             <label>Desired Username:</label>
-                            <input value={this.state.username} onChange={this.handleUserChange.bind(this)} type="text"/>
+                            <input value={this.state.username} onChange={this.handleUserChange.bind(this)} required="" type="text"/>
                             <label>Password:</label>
-                            <input value={this.state.password} onChange={this.handlePassChange.bind(this)} type="text"/>
+                            <input value={this.state.password} onChange={this.handlePassChange.bind(this)} required="" type="text"/>
                             <label>Confirm Password:</label>
-                            <input value={this.state.confirmPass} onChange={this.handleConfirm.bind(this)} type="text"/>
+                            <input value={this.state.confirmPass} onChange={this.handleConfirm.bind(this)} required="" type="text"/>
                             <button onClick={this.onSubmit.bind(this)}>Sign Up</button>
                         </form>
                     </div>
