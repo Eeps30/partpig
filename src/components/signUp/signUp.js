a0c7ac8f4a6b7946c8e84f46c0ab0b66ef9ca1f1
Eeps30 <eeps30@gmail.com>
2018-04-24 18:29:53 -0700

    completed sign up page

--- src/components/signUp/signUp.js
+++ src/components/signUp/signUp.js
@@ -1,29 +1,67 @@
 import React, {Component} from 'react';
+import './signUp.css';
 
 class SignUp extends Component {
     constructor(props){
         super(props)
             
         this.state = {
-            
+            email: '',
+            username: '',
+            password: '',
+            confirmPass: ''
         }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    handleUserChange(event){
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handlePassChange(event){
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleConfirm(event){
+        this.setState({
+            confirmPass: event.target.value
+        })
+    }
+
+    onSubmit(event){
+        event.preventDefault();
+        console.log('You submitted');
     }
 
     render(){
         return (
             <div>
-                <div className="container">
-                    <form action="">
-                        <h1 className="text-center">Sign Up for a Part Pig Account</h1>
-                        <label>Email Address:</label>
-                        <input type="text"/>
-                        <label>Desired Username:</label>
-                        <input type="text"/>
-                        <label>Password:</label>
-                        <input type="text"/>
-                        <label>Confirm Password:</label>
-                        <input type="text"/>
-                    </form>
+                <div className="outer-container">
+                    <div className="inner-container">
+                        <form>
+                            <h2>Create an Account</h2>
+                            <label>Email Address:</label>
+                            <input value={this.state.email} onChange={this.handleChange.bind(this)} type="text"/>
+                            <label>Desired Username:</label>
+                            <input value={this.state.username} onChange={this.handleUserChange.bind(this)} type="text"/>
+                            <label>Password:</label>
+                            <input value={this.state.password} onChange={this.handlePassChange.bind(this)} type="text"/>
+                            <label>Confirm Password:</label>
+                            <input value={this.state.confirmPass} onChange={this.handleConfirm.bind(this)} type="text"/>
+                            <button onClick={this.onSubmit.bind(this)}>Sign Up</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         )
