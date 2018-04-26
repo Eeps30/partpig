1e167ab6eb50ed9c83818acdc79018f32c9aeb44
unknown <briantmorr@gmail.com>
2018-04-26 12:30:32 -0700

    updated error component message

--- src/components/tools/errorHandling/error.js
+++ src/components/tools/errorHandling/error.js
@@ -1,38 +1,39 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './error.css';
 
 class AxiosError extends Component {
     constructor(props) {
         super(props);
-       
+
         this.onButtonClick = this.onButtonClick.bind(this);
     }
-        render(){
-            return  (
+    render() {
+        return (
 
-                <div className="errorContainer">     
-                    <div className="errorMessage">
-                        <div className="errorTitle">
+            <div className="errorContainer">
+                <div className="errorMessage">
+                    <div className="errorTitle">
                         <p id="errorHeader">Server Error</p>
-                        </div>
-                        <h1>Sorry, there was an error in retrieving your data</h1>
-                        <p>Part pig is still in beta, please leave us feedback about what broke and we will fix it!
-                        </p>
-                        <p>
-                             Thanks, 
-                            the Part Pig team
-                        </p>
-                        <button className="button-link feedbackBtn" onClick={this.onButtonClick}>Contact Us</button>
-                    
                     </div>
+                    <h1>Sorry! There was an error in retrieving your data.</h1>
+                    <p>Partpig is still in beta.</p>
+                    <p>Please leave us feedback about the problem you encountered and we will fix it as soon as possible!
+                        </p>
+                    <p>Thank you for your patience.</p>
+                    <p> Sincerely,
+                   the Part Pig team</p>
+                    <button className="button-link feedbackBtn errorButtonSpacing" onClick={this.onButtonClick}>Contact Us</button>
+                    <button className="button-link feedbackBtn errorButtonSpacing" onClick={this.props.history.goBack}>Go Back</button>
                 </div>
-            );
-            
-        }
-        onButtonClick(){
-            this.props.history.push('/contact');         }
+            </div>
+        );
+
     }
+    onButtonClick() {
+        this.props.history.push('/contact');
+    }
+}
 
 export default AxiosError;
