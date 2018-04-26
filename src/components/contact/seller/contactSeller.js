8a7eba4872784914be57c07fd40b1944f27a6b1f
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-04-25 21:41:37 -0700

    First round of folder restructuring done

--- src/components/contact/seller/contactSeller.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ContactSeller = () => (
-        <div>
-            <h1>This is the contact seller page</h1>
-        </div>    
-);
-
-export default ContactSeller
