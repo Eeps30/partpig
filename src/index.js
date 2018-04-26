289d6e48b639fadf54294ed9d960dd352df2023f
Li Zhang <bylizhang@gmail.com>
2018-04-26 10:51:04 -0700

    removed Provider from index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from "react-redux";
-import {createStore, applyMiddleware} from "redux";
-
 import App from './components/app/app';
 
-const store = createStore(rootReducer, {}, applyMiddleware());
-
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <App />,
     document.getElementById('root')
 );
