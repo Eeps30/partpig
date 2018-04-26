e591aafb266665238a9b40372be8f2077322fe16
Li Zhang <bylizhang@gmail.com>
2018-04-26 10:43:26 -0700

    Removed store and reducer from index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
-import {createStore, applyMiddleware} from "redux";
-
 import App from './components/app/app';
 
-const store = createStore(rootReducer, {}, applyMiddleware());
 
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider>
         <App />
     </Provider>,
     document.getElementById('root')
