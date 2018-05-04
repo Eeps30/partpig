96bf7239a44731357eb7c8fda0fcc1970522b201
Eeps30 <eeps30@gmail.com>
2018-05-04 11:51:12 -0700

    made items in footer just text instead of links to pages

--- src/components/app/footer.js
+++ src/components/app/footer.js
@@ -4,7 +4,10 @@ const Footer = () => (
  <footer>
     <div className="social"></div>
     <div className="legal">
-        <p><a href="useragreement.html">User Agreement</a><a href="cookiepolicy.html">Cookie Policy</a><a href="privacypolicy.html">Privacy Policy</a></p>
+        <p>User Agreement</p>
+        <p>Cookie Policy</p>
+        <p>Privacy Policy</p>
+            <br/>
         <p>Copyright © 2018 Part Pig All Rights Reserved.</p>
     </div>
  </footer>
