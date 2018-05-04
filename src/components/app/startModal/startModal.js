3de7c18376d56dfa406e1821a71da6c9e67b45a2
unknown <briantmorr@gmail.com>
2018-05-03 17:33:18 -0700

    modal styling and functions operational

--- src/components/app/startModal/startModal.js
+++ src/components/app/startModal/startModal.js
@@ -1,27 +1,36 @@
 
 import React, { Component } from "react";
 import './startModal.css';
+import '../../userDashboard/media4.css';
+
 
 class Modal extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        
+        this.addClickHandlers();
     }
-    hideModal(){
-        console.log('clicked');
-        document.getElementsByClassName('noResultModal').add.classList('hide');
+    addClickHandlers() {
+        window.onclick = () => this.hideModal();
+    }
+    hideModal() {
+        document.getElementsByClassName('startModal')[0].classList.add('hide');
+        document.getElementsByClassName('fadeOverlay')[0].classList.add('hide');
+        window.onclick = () => { };
     }
     render() {
-        return(
-        <div className="noResultModal toggleDisplay">
-            <div className="modalBox">
-                <div className="modalEscape" onClick={this.hideModal}>X</div>
+        return (
+            <div className="startModal">
+                <div className="modalEscape"></div>
                 <div className="text modalText">
-                    <h1>Welcome To part Pig Beta!</h1>
-                    <h3>Try searching Subaru impreza for best results</h3>
+                    <h1 className="modalTitle">Welcome To PartPig Beta!</h1>
+                    <hr/>
+                    <br/>
+                    <h3>For the best experience please search</h3>
+                    <h3>Make: Subaru, Model:Impreza WRX STI, Year: 2007</h3>
+                    <br/>
+                    <h3 className="modalProceed">Click anywhere to proceed!</h3>
                 </div>
             </div>
-        </div>
         );
     }
 }
