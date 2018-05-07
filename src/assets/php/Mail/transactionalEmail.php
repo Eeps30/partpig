cc921e6bf4fd3f6da21d7e1d84f2466b3a91d070
unknown <briantmorr@gmail.com>
2018-05-07 15:44:18 -0700

    removed cors in preparation for deployment

--- src/assets/php/Mail/transactionalEmail.php
+++ src/assets/php/Mail/transactionalEmail.php
@@ -1,8 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-
 require("../config/mailDetails.php");
 
 
